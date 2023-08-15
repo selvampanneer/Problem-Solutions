8be62b54c5c84fc80814312616b72d51cd064135
selvampanneer <54848562+selvampanneer@users.noreply.github.com>
2023-08-15 23:43:02 +0530

    Update Q4.c
    LCM of 10<= n <= 100 numbers.
    Approach:
    1.Find the prime numbers between the specified range and multiply them.
    2.Find the maximum power of all prime numbers that lie between the lower and upper bound of the range given. If the prime number is already included a time in step 1, you need to divide them or make changes to step 1 accordingly.
    3.gmp library is used to compute larger numbers like this problem.

--- ADSAT-LAB/WEEK0/Q4.c
+++ ADSAT-LAB/WEEK0/Q4.c
@@ -10,7 +10,7 @@ bool isPrime(int n)
     if (n <= 1 || n % 2 == 0 || n % 3 == 0)
         return false;
 
-    // To check through all numbers of the form 6k ± 1(new approach)
+    //To check through all numbers of the form 6k ± 1(new approach)
     for (int i = 5; i * i <= n; i += 6)
     {
         if (n % i == 0 || n % (i + 2) == 0)
