ce473fcc323f6e541be25f791da3890435e85341
selvampanneer <54848562+selvampanneer@users.noreply.github.com>
2023-08-15 23:40:05 +0530

    Create Q4.c
    LCM of 10&lt;= n &gt;= 100 numbers.
    Approach:
    1.Find the prime numbers between the specified range and multiply them.
    2.Find the maximum power of all prime numbers that lie under the upper bound of the range given. If the prime number is already included a time in step 1, you need to divide them or make changes to step 1 accordingly.
    3.gmp library is used to compute larger numbers like this problem.

---
+++ ADSAT-LAB/WEEK0/Q4.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<stdbool.h>
+#include<gmp.h>
+#include<math.h>
+bool isPrime(int n)
+{
+    if (n == 2 || n == 3)
+        return true;
+
+    if (n <= 1 || n % 2 == 0 || n % 3 == 0)
+        return false;
+
+    // To check through all numbers of the form 6k ± 1(new approach)
+    for (int i = 5; i * i <= n; i += 6)
+    {
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+    }
+
+    return true;
+}
+int main()
+{
+    long int i,j,temp=1;
+    mpz_t sum;
+    mpz_init_set_ui(sum,1);
+    bool flag;
+    for( i=10; i<=100; i++)
+    {
+        flag=false;
+        flag = isPrime(i);
+        if(flag==true)
+        {
+            mpz_mul_ui(sum,sum,i);
+        }
+    }
+    for(i=0; i<sqrt(100); i++)
+    {
+        temp = 1;
+        if(isPrime(i))
+        {
+            while(temp<=100)
+            {
+                temp*=i;
+                mpz_mul_ui(sum,sum,i);
+            }
+        }
+    }
+    gmp_printf("%Zd",sum);
+    mpz_clear(sum);
+
+    return 0;
+}
